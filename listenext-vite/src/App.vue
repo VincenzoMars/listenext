ade0793b49c911741883f0d8b46c98b8cffc3804
MVRS <vincenzo.mars16@gmail.com>
2022-06-18 14:40:12 +0200

    import from portfolio-template

---
+++ listenext-vite/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <splash-loader></splash-loader>
+  <router-view></router-view>
+</template>
+
+<script>
+import SplashLoader from './components/SplashLoader.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SplashLoader
+  }
+} 
+</script>
